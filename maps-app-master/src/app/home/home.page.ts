f0149de00783e19e942dd6f71d22b19911e10058
Pedro h <pedro.henrique.lima0205@gmail.com>
2023-10-31 20:29:08 -0300

    31-10

--- maps-app-master/src/app/home/home.page.ts
+++ maps-app-master/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Component({
@@ -14,7 +14,15 @@ export class HomePage {
   // Cria a variavel do Maps
   map!: google.maps.Map;
 
-  constructor() {}
+  listaEnderecos: google.maps.places.AutocompletePrediction[] = [];
+
+  private autoComplete = new google.maps.places.AutocompleteService();
+  private directions = new google.maps.DirectionsService();
+  private directionsRender = new google.maps.DirectionsRenderer();
+
+  minhaPosicao!: google.maps.LatLng; //Guarda nossa posição;
+
+  constructor(private ngZone: NgZone) {}
 
   async exibirMapa(){
 
@@ -47,27 +55,80 @@ export class HomePage {
     const coordinates = await Geolocation.getCurrentPosition();
     console.log('Current position:', coordinates);
 
-    const position = {
+    this.minhaPosicao = new google.maps.LatLng ({
       lat: coordinates.coords.latitude,
       lng: coordinates.coords.longitude
-    }
-    this.map.setCenter(position);
+  });
+    this.map.setCenter(this.minhaPosicao);
     this.map.setZoom(18);
 
-    this.adicionarMarcador(coordinates);
+    this.adicionarMarcador(this.minhaPosicao);
   }
 
-  async adicionarMarcador(localizacao: Position){
+  async adicionarMarcador(localizacao: google.maps.LatLng){
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
      //The marker, positioned at Uluru
     const marker = new AdvancedMarkerElement({
     map: this.map,
-    position: {
-      lat: localizacao.coords.latitude,
-      lng: localizacao.coords.longitude
-    },
-    title: 'Uluru',
+    position: localizacao,
+    title: 'Posição',
+    });
+  }
+
+  //buscar os endereços no MAPS
+  buscarEnderecos(valorBuscar: any){
+    const busca = valorBuscar.target.value as string;
+
+    //Confere se ele não esta vazio
+    //Lembrando que  0 é igual a false
+    if(!busca.trim().length){
+      this.listaEnderecos = []; //Limpa a lista se estiver vazia
+      return false; //encerra o método.
+    }
+
+    this.autoComplete.getPlacePredictions(
+      {input: busca}, // envia os dados para busca
+      (arrayLocais, status) => { //variaveis com o retorno
+        if(status == 'OK'){ //Se o retorno estiver OK
+          this.ngZone.run(()=>{ //Atualiza as diretivas de tela
+            this.listaEnderecos = arrayLocais ? arrayLocais : [];
+            console.log(this.listaEnderecos);
+          });
+        }else{
+          //Se o retorno deu erro, limpa a lista
+          this.listaEnderecos = [];
+        }
+      }
+    );
+    return true;
+  }
+
+  public tracarRota(local: google.maps.places.AutocompletePrediction){
+    this.listaEnderecos = []; //Limpa a lista
+
+    //Converte o endereço de texto para posição GPS
+    new google.maps.Geocoder().geocode({address: local.description}, resultado=>{
+      const localizacao = resultado![0].geometry.location;
+
+      this.adicionarMarcador(localizacao); //Adiciona o marcador no novo local
+
+      //Cria uma rota
+      const rota: google.maps.DirectionsRequest = {
+        origin: this.minhaPosicao,
+        destination: resultado![0].geometry.location,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+
+      //Desenha a rota no mapa
+      this.directions.route(rota, (resultado, status)=>{
+        if(status == 'OK'){
+          this.directionsRender.setMap(this.map);
+          this.directionsRender.setOptions({suppressMarkers: true});
+          this.directionsRender.setDirections(resultado);
+        }
+      });
     });
   }
 
