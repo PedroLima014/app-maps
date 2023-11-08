76d41d2f4f1b9906eda9078b5f0eda6f90cf84bb
Pedro h <pedro.henrique.lima0205@gmail.com>
2023-11-07 21:04:25 -0300

    07-11u

--- maps-app-master/src/app/home/home.page.ts
+++ maps-app-master/src/app/home/home.page.ts
@@ -29,12 +29,8 @@ export class HomePage {
     // The location of Uluru
     const position = { lat: -22.489201, lng: -48.546444 };
 
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-
     // The map, centered at Uluru
-    this.map = new Map(
+    this.map = new google.maps.Map(
       this.mapRef.nativeElement,
       {
         zoom: 20,
@@ -66,10 +62,9 @@ export class HomePage {
   }
 
   async adicionarMarcador(localizacao: google.maps.LatLng){
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
      //The marker, positioned at Uluru
-    const marker = new AdvancedMarkerElement({
+    const marker = new google.maps.Marker({
     map: this.map,
     position: localizacao,
     title: 'Posição',
